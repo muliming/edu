902293e226c06d65067101662ef11795f71b8629
muliming <55885581+muliming@users.noreply.github.com>
2021-02-02 10:54:49 +0800

    条件加分页查询讲师

--- service/service_edu/src/main/java/com/lcj/eduService/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/lcj/eduService/controller/EduTeacherController.java
@@ -1,15 +1,21 @@
 package com.lcj.eduService.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.lcj.commonutils.R;
 import com.lcj.eduService.entity.EduTeacher;
+import com.lcj.eduService.entity.vo.TeacherQuery;
 import com.lcj.eduService.service.EduTeacherService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -47,6 +53,71 @@ public class EduTeacherController {
     }
 
 
+    /**
+     * @param current 当前页
+     * @param limit 每页记录数
+     * @return
+     */
+    @GetMapping("pageTeacher/{current}/{limit}")
+    public R pageListTeacher(@PathVariable long current,
+                             @PathVariable long limit){
+
+        //创建page对象
+        Page<EduTeacher> pageTeacher=new Page<>(current,limit);
+        //调用方法时，底层封装，把分页数据封装到pageTeacher对象里面
+        eduTeacherService.page(pageTeacher,null);
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records=pageTeacher.getRecords();
+        Map map=new HashMap();
+        map.put("total",total);
+        map.put("row",records);
+        return R.ok().data(map);
+    }
+
+
+    /**
+     * 条件查询
+     * @param current
+     * @param limit
+     * @param teacherQuery
+     * @return
+     */
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageListTeacher(@PathVariable long current,
+                             @PathVariable long limit,
+                             @RequestBody(required = false) TeacherQuery teacherQuery){
+        //创建一个配置对象
+        Page<EduTeacher> pageTeacher=new Page<>(current,limit);
+        //构造条件
+        QueryWrapper<EduTeacher> wrapper=new QueryWrapper<>();
+        //判断条件是否为空，如果不为空拼接条件
+        String name=teacherQuery.getName();
+        String level=teacherQuery.getLevel();
+        String begin=teacherQuery.getBegin();
+        String end=teacherQuery.getEnd();
+        if(!StringUtils.isEmpty(name)){
+            wrapper.like("name",name);
+        }
+
+        if(!StringUtils.isEmpty(level)){
+            wrapper.eq("level",level);
+        }
+
+        if(!StringUtils.isEmpty(begin)){
+            wrapper.ge("gnt_create",begin);
+        }
+
+        if(!StringUtils.isEmpty(end)){
+            wrapper.le("gnt_create",end);
+        }
+        eduTeacherService.page(pageTeacher,wrapper);
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records=pageTeacher.getRecords();
+        Map map=new HashMap();
+        map.put("total",total);
+        map.put("row",records);
+        return R.ok().data(map);
+    }
 
 
 
@@ -56,5 +127,6 @@ public class EduTeacherController {
 
 
 
-}
+
+    }
 
