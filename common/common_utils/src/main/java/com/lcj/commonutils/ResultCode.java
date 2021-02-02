bac6a2e5904e91f4f7e84a936a8ba949fc71ea82
muliming <55885581+muliming@users.noreply.github.com>
2021-02-02 09:52:11 +0800

    统一返回结果

---
+++ common/common_utils/src/main/java/com/lcj/commonutils/ResultCode.java
@@ -0,0 +1,13 @@
+package com.lcj.commonutils;
+
+/**
+ * @Author：LCJ
+ * @Description：
+ * @Date：Create in 9:29 2021/2/2 0002
+ */
+public interface ResultCode {
+
+    public static Integer SUCCESS = 20000; //成功
+
+    public static Integer ERROR = 20001; //失败
+}
--- service/service_edu/src/main/java/com/lcj/eduService/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/lcj/eduService/controller/EduTeacherController.java
@@ -1,6 +1,7 @@
 package com.lcj.eduService.controller;
 
 
+import com.lcj.commonutils.R;
 import com.lcj.eduService.entity.EduTeacher;
 import com.lcj.eduService.service.EduTeacherService;
 import io.swagger.annotations.Api;
@@ -29,17 +30,20 @@ public class EduTeacherController {
     //查询所有的讲师
     @ApiOperation(value = "所有讲师列表")
     @GetMapping("findAll")
-    public List<EduTeacher> findAllTeacher(){
+    public R findAllTeacher(){
         List<EduTeacher> list=eduTeacherService.list(null);
-        return list;
+        return R.ok().data("teachers",list);
     }
 
     //讲师逻辑删除
     @ApiOperation(value = "逻辑删除讲师")
     @DeleteMapping("{id}")
-    public boolean removeTeacher(@PathVariable String id){
+    public R removeTeacher(@PathVariable String id){
         boolean flag=eduTeacherService.removeById(id);
-        return flag;
+        if(!flag){
+            return R.error();
+        }
+        return R.ok();
     }
 
 
